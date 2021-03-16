7600384cad18bb5da651f011a5a5720bd802f9c4
adnane <adnane.bachchar@gmail.com>
2021-03-16 09:56:28 +0100

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -148,9 +148,6 @@ app.use("/Cart", (req, res) => {
 });
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("API is running ....");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/productsList", productListRoutes);
@@ -160,6 +157,18 @@ app.use("/api/orders", orderRoutes);
 
 const __dirname = path.resolve();
 app.use("frontend/public/uploads", express.static(path.join(__dirname)));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(NotFound);
 
 app.use(ErrorHandler);
